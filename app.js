09cff71ff899dbcdfca192ed9afa0779c735a648
gone-2-plaid <cory.vanderpoel@gmail.com>
2026-03-31 21:14:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,41 @@
+// Clean a string for filenames: replace spaces and slashes with underscores
+function clean(x) {
+    return x.replace(/[\/\s]+/g, "_");
+}
+
+// Initial model values
 var myAppData = {
-    // Dropdown options (cleaned for filenames)
-    crimeGroupOptions: [
-        "Drug_Vice",
-        "Other",
-        "Property",
-        "Public Order",
-        "Violent",
-        "White Collar"
-    ],
-
-    locationGroupOptions: [
-        "Commercial Retail",
-        "Entertainment",
-        "Government_Institutional",
-        "Industrial_Warehouse",
-        "Other_Unknown",
-        "Parks_Outdoor",
-        "Residential",
-        "School_Childcare",
-        "Street_Sidewalk",
-        "Transit_Vehicle"
-    ],
-
-    // Default values
     crimeGroup: "Drug_Vice",
     locationGroup: "Residential",
     year: 2001,
-
-    // ⭐ REQUIRED: initialize the reactive image variable
     heatmapSrc: ""
 };
 
-// --- THIS IS THE KEY CHANGE ---
-// Create a TangleModel instance for your data
+// Create the model
 var myTangleModel = new TangleModel(myAppData);
 
-// Add an observer to update heatmapSrc whenever model values change
-myTangleModel.addObserver(function() {
+// Update heatmap whenever any variable changes
+myTangleModel.addObserver(function () {
     var filename =
-        myTangleModel.get("crimeGroup") + "_" +
-        myTangleModel.get("locationGroup") + "_" +
+        clean(myTangleModel.get("crimeGroup")) + "_" +
+        clean(myTangleModel.get("locationGroup")) + "_" +
         myTangleModel.get("year") + ".png";
+
     myTangleModel.set("heatmapSrc", "heatmaps/" + filename);
+
+    document.getElementById("heatmap").src = myTangleModel.get("heatmapSrc");
 });
 
-// Initialize the heatmapSrc for the first time
-// This needs to be done *after* the model is set up and the observer is added
-var initialFilename =
-    myTangleModel.get("crimeGroup") + "_" +
-    myTangleModel.get("locationGroup") + "_" +
-    myTangleModel.get("year") + ".png";
-myTangleModel.set("heatmapSrc", "heatmaps/" + initialFilename);
-
-
+// Initialize Tangle after page loads
 window.onload = function () {
-    // Initialize Tangle on the specific #controls div
-    // And pass your TangleModel instance
-    new Tangle(document.getElementById('controls'), myTangleModel);
+    new Tangle(document.getElementById("controls"), myTangleModel);
 
-    // Also, ensure the img#heatmap has a data-class if you want Tangle to manage its src
-    // However, the current Tangle core does not have a TKImg or similar class to set src
-    // So, we will manually set it via the TangleModel observer.
-    // If you add a Tangle.classes.TKImage, then you could change img#heatmap to:
-    // <img id="heatmap" data-var="heatmapSrc" data-class="TKImage" alt="Heatmap will appear here">
-    // For now, the observer approach is robust.
-    document.getElementById('heatmap').src = myTangleModel.get("heatmapSrc");
+    // Set initial heatmap
+    var initialFilename =
+        clean(myTangleModel.get("crimeGroup")) + "_" +
+        clean(myTangleModel.get("locationGroup")) + "_" +
+        myTangleModel.get("year") + ".png";
+
+    myTangleModel.set("heatmapSrc", "heatmaps/" + initialFilename);
+    document.getElementById("heatmap").src = myTangleModel.get("heatmapSrc");
 };
